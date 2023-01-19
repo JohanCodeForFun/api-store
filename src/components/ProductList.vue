1f6ad0eb3c2a07510c833568c468c51524afd224
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-19 11:46:36 +0100

    add orderterms for products sorting

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -12,14 +12,28 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 import Product from '../types/Product'
+import OrderTermProducts from '../types/OrderTermProducts';
 
 export default defineComponent({
 	props: {
 		products: {
 			type: Array as PropType<Product[]>
-		}
+		},
+		order: {
+			required: true,
+			type: String as PropType<OrderTermProducts>
+		},
+		// setup(props) {
+		// 	const orderedProducts = computed(() => {
+		// 		return [...props.products].sort((a: Product, b:Product) => {
+		// 			return a[props.order] > b[props.order] ? 1 : -1
+		// 		})
+		// 	})
+
+		// 	return { orderedProducts }
+		// } 
 	}
 })
 </script>
