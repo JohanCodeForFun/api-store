36791deb86a5d9e8fc6088bb72bb23b2d2848737
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-06 22:21:51 +0100

    add pinia and bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './assets/global.css'
 import App from './App.vue'
+import { createPinia } from "pinia";
+import 'bootstrap/dist/css/bootstrap.css'
+// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
-createApp(App).mount('#app')
+createApp(App)
+// .use(bootstrap)
+.use(createPinia())
+.mount('#app')
