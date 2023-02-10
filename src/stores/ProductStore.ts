fc4b67de35b9a114ca0ca880ef47a1642eae940e
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-10 11:07:19 +0100

    change file ending

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -1,12 +1,13 @@
 import { defineStore } from "pinia";
-import products from '../App.vue'
+import products from "../App.vue";
+import Product from "../types/Product";
 
-export const useProductStore = defineStore('ProductStore', {
-	state: () => {
-		return {
-			products,
-		}
-	}
-	// actions 
-	// getters
-})
+export const useProductStore = defineStore("ProductStore", {
+  state: () => {
+    return {
+      products,
+    };
+  },
+  // actions
+  // getters
+});
