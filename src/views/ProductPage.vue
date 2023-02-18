13ffeb1721bfd08c081853623dd5d1a7906693e3
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-18 22:31:10 +0100

    write to-do list for product page

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -2,11 +2,21 @@
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<h3>Product page</h3>
-				<h3>{{ product.title }}</h3>
-				<p>{{ $route.params.id }}</p>
+
 				<!-- show main product after route -->
-				<!-- show other recommended products -->
+
+				<h3>Product page</h3>
+				<!-- <h3>{{ product.title }}</h3> -->
+				<p>Route Param, {{ $route.params.id }}</p>
+				<p>Product id, { here }</p>
+
+				<p>// show detailed product info</p>
+				<p>// add add to cart button</p>
+				<p>// add router link for each product in component ProductList to push to, product/:id</p>
+
+				<h4>Other products in category...</h4>
+				<!-- show other products in same category -->
+			
 			</div>
 		</div>
 	</div>
