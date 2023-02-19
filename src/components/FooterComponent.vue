28d7b54f275e39786db6e2976619144946a4b920
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-19 17:57:55 +0100

    add created and link to portfolio site in footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,7 +3,9 @@
 		<div class="row">
 			<div class="col">
 				<h3 class="text-center">Footer</h3>
-				<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<p class="text-center">Created by Johan Hellberg 2023. Website, <a href="https://jhellberg.com"
+						target="_blank">jhellberg.com</a>
+				</p>
 			</div>
 		</div>
 	</div>
