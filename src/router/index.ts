b5f487cca908792f54eb1580af7b95cc1c825870
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-19 17:24:33 +0100

    add routing params id to ProductPage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/HomePage.vue';
-import About from '../views/AboutPage.vue';
-import Product from '../views/ProductPage.vue';
-import NotFound from '../views/PageNotFound.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/HomePage.vue";
+import About from "../views/AboutPage.vue";
+import Product from "../views/ProductPage.vue";
+import NotFound from "../views/PageNotFound.vue";
 
 const routes = [
-	{ path: '/', name: 'Home', component: Home },
-	{ path: '/about', name: 'About', component: About },
-	{ path: '/product/:id', name: 'Product', component: Product },
-	{ path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
-]
+  { path: "/", name: "Home", component: Home },
+  { path: "/about", name: "About", component: About },
+  { path: "/product/:id", name: "Product", component: Product },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+];
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,39 +1,69 @@
 <template>
-	<div class="container">
+	<main class="container">
+
 		<div class="row">
 			<div class="col">
 
 				<!-- show main product after route -->
+
+				<!-- working on getting product from route param -->
+				{{ getSingleProduct }}
 
 				<h3>Product page</h3>
 				<!-- <h3>{{ product.title }}</h3> -->
 				<p>Route Param, {{ $route.params.id }}</p>
 				<p>Product id, { here }</p>
 
-				<p>// show detailed product info</p>
-				<p>// add add to cart button</p>
-				<p>// add router link for each product in component ProductList to push to, product/:id</p>
+				<ul>
+					<h3>TO DO:</h3>
+					<li>
+						<p>Show detailed product info</p>
+					</li>
+					<li>
+						<p>Sdd add to cart button</p>
+					</li>
+					<li>
+						<p>In component ProductList: add router link for each product to push to, product/:id</p>
+					</li>
+					<li>
+						<p>in SearchBar, move filter to getter from actions</p>
+					</li>
+					<li>
+						<p>in App.vue, Move SearchBar to only show in HomePage</p>
+					</li>
+					<li>
+						<p>+ fix fixed navbar on top !</p>
+					</li>
+					<li>
+						<p>Bonus, add skeleton loader getting products at HomePage.vue</p>
+					</li>
+				</ul>
+
 
 				<h4>Other products in category...</h4>
 				<!-- show other products in same category -->
-			
+
 			</div>
 		</div>
-	</div>
+	</main>
 </template>
 
 <script lang="ts">
+import { computed, defineComponent } from 'vue';
+import { useProductStore } from '../stores/ProductStore'
 
-import products from '../App.vue'
+export default defineComponent({
+	setup() {
+		const productStore = useProductStore();
 
-export default {
-	computed: {
-		productId() {
-			return parseInt(this.$route.params.id as string)
-		},
-		// product() {
-		// 	return products.products.find(obj as number => obj.id === this.productId)
-		// }
-	}
-}
+		const productId = computed(() => {
+			return parseInt(this.$route.params.id)
+		});
+
+		const getSingleProduct = computed(() => {
+			return productStore.filteredProducts.find(product => product.id === this.$route.params.id)
+		});
+		return { useProductStore, productId, getSingleProduct }
+	},
+})
 </script>
