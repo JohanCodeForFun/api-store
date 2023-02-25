9363e05b91f0db4e2dc9a75a2aeca2db28cc82cc
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-25 18:04:03 +0100

    show specific product after navigation

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -6,11 +6,30 @@
 
 				<!-- show main product after route -->
 				<!-- working on getting product from route param -->
-				{{ getSingleProduct }}
-
-				<h3>Product page</h3>
-				<!-- <h3>{{ product.title }}</h3> -->
-				<p>Product id, { here }</p>
+				<div class="card" style="width: 18rem;">
+					<a href="#">
+						<img :src="product?.image" class="card-img-top" :alt="product?.title">
+					</a>
+					<div class="card-body">
+						<h5 class="card-title">{{ product?.title }}</h5>
+						<p class="card-text">Review: {{ product?.rating.rate }}</p>
+						<div class="row align-items-center">
+							<div class="col-3">
+								<RouterLink :to="`/product/${product?.id}`">
+									<button class="btn btn-primary">
+										<i class="fa-solid fa-circle-info fa-xl"></i>
+									</button>
+								</RouterLink>
+							</div>
+							<div class="col-5">
+								<button class="btn btn-success" @click="cartStore.addItems(product)">Buy Now</button>
+							</div>
+							<div class="col-4">
+								<h5 class="card-text">${{ product?.price }}</h5>
+							</div>
+						</div>
+					</div>
+				</div>
 
 				<h4>Other products in category...</h4>
 				<!-- show other products in same category -->
@@ -22,20 +41,27 @@
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue';
+import { useRoute } from 'vue-router';
 import { useProductStore } from '../stores/ProductStore'
+import { useCartStore } from '../stores/CartStore';
 
 export default defineComponent({
+	emits: ['addToCart'],
 	setup() {
+		const route = useRoute();
 		const productStore = useProductStore();
+		const cartStore = useCartStore();
 
-		const productId = computed(() => {
-			return parseInt(this.$route.params.id)
-		});
+		const product = productStore.filteredProducts.find(product => product.id === parseInt(route.params.id))
 
-		const getSingleProduct = computed(() => {
-			return productStore.filteredProducts.find(product => product.id === this.$route.params.id)
-		});
-		return { useProductStore, productId, getSingleProduct }
+		return { useProductStore, cartStore, product }
 	},
 })
 </script>
+
+<style>
+.product-image {
+	width: 300px;
+	height: auto;
+}
+</style>
